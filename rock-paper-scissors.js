a9d46a9fea8781d4a0fa2790661a1b49310a470a
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-01-24 14:14:31 -0700

    feat: create getComputerChoice()
    Added the logic to get the computer choice.
    The computer randomly returns a string "rock",
    "paper", or "scissors".

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,12 @@
 console.log("Welcome to the Rock-Paper-Scissors Game!");
+
+// the three choices that can be made both by the player or the computer
+const choices = ["rock", "paper", "scissors"];
+
+// function that randomly returns rock, paper, or scissors for the computer
+function getComputerChoice() {
+  return choices[ Math.floor( Math.random() * 3 ) ];
+}
+
+let computerChoice = getComputerChoice();
+console.log("The computer chose: " + computerChoice);
