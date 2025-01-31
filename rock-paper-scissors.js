6db03731df3718f224251ee7d482ef495bac4a61
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-01-31 14:56:22 -0700

    fix: edit getHumanChoice() to incorporate UI event
    Refactored getHumanChoice() so that the game can be
    proceeded only when one of the three buttons is
    selected.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,46 +6,50 @@ const choices = ["rock", "paper", "scissors"];
 // the variables to keep track of the game status
 let humanScore = 0;
 let computerScore = 0;
-const maximumGamePlayable = 5;
+// const maximumGamePlayable = 5;
 let gamesPlayed = 0;
 let continuePlay;
 
 // function that randomly returns rock, paper, or scissors for the computer
 function getComputerChoice() {
-  return choices[ Math.floor( Math.random() * 3 ) ];
+  return choices[Math.floor(Math.random() * 3)];
 }
+
+// storing callbacks for click events
 
 // function that allows the player to choose rock, paper, or scissors
-async function getHumanChoice() {
-  // let enteredString = prompt("Please choose rock, paper, or scissors: ");
-  // let humanChoice = enteredString.toLowerCase();  // "sanitized" the input by making the string lowercase
-  const buttonContainer = document.querySelector(".choices");
-  const result = await holdUntilClick(buttonContainer);
-  return result;
-}
+function getHumanChoice() {
+  alert(
+    "You need to click one of the three choices. Otherwise, the game won't proceed!"
+  );
+  return new Promise((resolve) => {
+    const buttons = document.querySelectorAll(".button");
 
-async function holdUntilClick(buttonContainer) {
-  buttonContainer.addEventListener("click", (evt) => {
-    let target = evt.target;
-    return target.id;
+    function handleClick(evt) {
+      const choice = evt.currentTarget.id;
+      buttons.forEach((btn) => btn.removeEventListener("click", handleClick));
+      resolve(choice);
+    }
+
+    buttons.forEach((button) => button.addEventListener("click", handleClick));
   });
 }
 
 // "sleep" effect: holding the process for specified ms
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // "animation" effect: prints out "Let's Go!!!", "rock", "paper", "scissors" by one second
 async function countdown(arr) {
   console.log("Let's Go!!!");
   await sleep(1000);
-  for(elem of arr) {
+  for (elem of arr) {
     console.log(elem);
     await sleep(1000);
   }
 
-  return new Promise(resolve => resolve("Hey!"));
+  return new Promise((resolve) => resolve("Hey!"));
 }
 
 // function that plays a single round of rock paper scissors game
@@ -57,17 +61,19 @@ function playRound(humanChoice, computerChoice) {
 
   // if the human's choice is other than "rock", "paper", or "scissors", the index would be -1
   if (humanChoiceIndex === -1) {
-    console.log("You chose other than \'rock\', \'paper\', or \'scissors\'. The game cannot be proceeded.");
+    console.log(
+      "You chose other than 'rock', 'paper', or 'scissors'. The game cannot be proceeded."
+    );
     indexDifference = -999;
     return indexDifference;
   }
-  
+
   switch (indexDifference) {
     // if the indexes are the same, that means their choices are the same
     case 0:
       console.log(`Draw! Both of you chose ${humanChoice}`);
       break;
-    // according to the array layout, a player's win if the diffrence is either 1 or -2  
+    // according to the array layout, a player's win if the diffrence is either 1 or -2
     case 1:
     case -2:
       console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
@@ -91,10 +97,13 @@ function renderStatus() {
   let computerScoreElement;
   let finalResultElement;
 
-  if ( gamesPlayed === 0 ) {
+  if (gamesPlayed === 0) {
     gamesPlayedElement = renderTextElement(gameStatusContainer, "games-played");
     humanScoreElement = renderTextElement(gameStatusContainer, "human-score");
-    computerScoreElement = renderTextElement(gameStatusContainer, "computer-score");
+    computerScoreElement = renderTextElement(
+      gameStatusContainer,
+      "computer-score"
+    );
   } else {
     gamesPlayedElement = document.querySelector(".status #games-played");
     humanScoreElement = document.querySelector(".status #human-score");
@@ -107,10 +116,15 @@ function renderStatus() {
 
   if (!continuePlay) {
     finalResultElement = renderTextElement(gameStatusContainer, "final-result");
-    finalResultElement.textContent = `${(humanScore - computerScore > 0) ? "You won!" : (humanScore === computerScore ? "Draw!" : "You lost!" )} `;
+    finalResultElement.textContent = `${
+      humanScore - computerScore > 0
+        ? "You won!"
+        : humanScore === computerScore
+        ? "Draw!"
+        : "You lost!"
+    } `;
     finalResultElement.textContent += "Thank you for visiting us😇.";
   }
-
 }
 
 // render text elements with "id=idName" and append it to parentElement
@@ -123,8 +137,11 @@ function renderTextElement(parentElement, idName) {
 
 // function that plays the entire game. five rounds are played.
 async function playGame() {
-  
-  while(humanScore < 5 && computerScore < 5 && (continuePlay = confirm("Do you want to play rock-paper-scissors?"))) {
+  while (
+    humanScore < 5 &&
+    computerScore < 5 &&
+    (continuePlay = confirm("Do you want to play rock-paper-scissors?"))
+  ) {
     renderStatus();
 
     const computerChoice = getComputerChoice();
@@ -134,10 +151,10 @@ async function playGame() {
     // calling the "animation" function
     let promiseResult = await countdown(choices);
     console.log(promiseResult);
-    
+
     // separated the game logic and the UI by moving the score tracking to the UI
     const result = playRound(humanChoice, computerChoice);
-    
+
     if (result === 1 || result === -2) {
       humanScore++;
     }
@@ -147,13 +164,9 @@ async function playGame() {
     }
 
     gamesPlayed++;
-
   }
 
   renderStatus();
-
 }
 
 playGame();
-
-
