b5b8207f0111c7e13e2a62d18eab12ad977816e2
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-01-31 18:20:32 -0700

    fix: refactor countDown() and edit HTML and CSS
    Refactored countDown() to incorporate the UI by
    rendering DOM elements as per specifications.
    Also renamed the function to be renderAnimation().
    
    In addition, some HTML and CSS changes were made,
    including the import of Roboto and Roboto Mono fonts
    from Google.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,7 +6,6 @@ const choices = ["rock", "paper", "scissors"];
 // the variables to keep track of the game status
 let humanScore = 0;
 let computerScore = 0;
-// const maximumGamePlayable = 5;
 let gamesPlayed = 0;
 let continuePlay;
 
@@ -14,8 +13,6 @@ let continuePlay;
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
-
-// storing callbacks for click events
 
 // function that allows the player to choose rock, paper, or scissors
 function getHumanChoice() {
@@ -41,52 +38,17 @@ function sleep(ms) {
 }
 
 // "animation" effect: prints out "Let's Go!!!", "rock", "paper", "scissors" by one second
-async function countdown(arr) {
-  console.log("Let's Go!!!");
+async function renderAnimation(arr) {
+  const animationContainer = document.querySelector(".animation");
+  let animationTextElement = renderTextElement(animationContainer, "animation-text");
+  animationTextElement.textContent = "Let's Go!!!";
   await sleep(1000);
   for (elem of arr) {
-    console.log(elem);
+    animationTextElement.textContent = `${elem}`;
     await sleep(1000);
   }
 
-  return new Promise((resolve) => resolve("Hey!"));
-}
-
-// function that plays a single round of rock paper scissors game
-function playRound(humanChoice, computerChoice) {
-  // gets the index of rock, paper, and scissors in the array choices
-  const humanChoiceIndex = choices.indexOf(humanChoice);
-  const computerChoiceIndex = choices.indexOf(computerChoice);
-  let indexDifference = humanChoiceIndex - computerChoiceIndex;
-
-  // if the human's choice is other than "rock", "paper", or "scissors", the index would be -1
-  if (humanChoiceIndex === -1) {
-    console.log(
-      "You chose other than 'rock', 'paper', or 'scissors'. The game cannot be proceeded."
-    );
-    indexDifference = -999;
-    return indexDifference;
-  }
-
-  switch (indexDifference) {
-    // if the indexes are the same, that means their choices are the same
-    case 0:
-      console.log(`Draw! Both of you chose ${humanChoice}`);
-      break;
-    // according to the array layout, a player's win if the diffrence is either 1 or -2
-    case 1:
-    case -2:
-      console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-      // humanScore++;
-      break;
-    // everything else(0 or -1), a computer's win
-    default:
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
-      // computerScore++;
-      break;
-  }
-
-  return indexDifference;
+  return new Promise((resolve) => resolve("Got result!"));
 }
 
 // function that renders the game status
@@ -114,7 +76,7 @@ function renderStatus() {
   humanScoreElement.textContent = `Score for player: ${humanScore}`;
   computerScoreElement.textContent = `Score for computer: ${computerScore}`;
 
-  if (!continuePlay) {
+  if (!continuePlay || humanScore >= 5 || computerScore >= 5) {
     finalResultElement = renderTextElement(gameStatusContainer, "final-result");
     finalResultElement.textContent = `${
       humanScore - computerScore > 0
@@ -135,6 +97,59 @@ function renderTextElement(parentElement, idName) {
   return element;
 }
 
+// clears the animation and the result after each round
+function clear() {
+  const animationContainer = document.querySelector(".animation");
+  const resultContainer = document.querySelector(".result");
+  
+  while (animationContainer.firstChild || resultContainer.firstChild) {
+    animationContainer.removeChild(animationContainer.firstChild);
+    resultContainer.removeChild(resultContainer.firstChild);
+  }
+
+}
+
+// function that plays a single round of rock paper scissors game
+function playRound(humanChoice, computerChoice) {
+  // gets the index of rock, paper, and scissors in the array choices
+  const humanChoiceIndex = choices.indexOf(humanChoice);
+  const computerChoiceIndex = choices.indexOf(computerChoice);
+  let indexDifference = humanChoiceIndex - computerChoiceIndex;
+  const resultContainer = document.querySelector(".result");
+  let resultElement = renderTextElement(resultContainer, "result-text");
+
+  // if the human's choice is other than "rock", "paper", or "scissors", the index would be -1
+  if (humanChoiceIndex === -1) {
+    // console.log(
+    //   "You chose other than 'rock', 'paper', or 'scissors'. The game cannot be proceeded."
+    // );
+    resultElement.textContent = "You chose other than \'rock\', \'paper\', or \'scissors\'. The game cannot be proceeded."
+    indexDifference = -999;
+    return indexDifference;
+  }
+
+  switch (indexDifference) {
+    // if the indexes are the same, that means their choices are the same
+    case 0:
+      // console.log(`Draw! Both of you chose ${humanChoice}`);
+      resultElement.textContent = `Draw! Both of you chose ${humanChoice}`;
+      break;
+    // according to the array layout, a player's win if the diffrence is either 1 or -2
+    case 1:
+    case -2:
+      // console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+      resultElement.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
+      break;
+    // everything else(0 or -1), a computer's win
+    default:
+      // console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+      resultElement.textContent = `You lose! ${computerChoice} beats ${humanChoice}!`;
+      break;
+  }
+
+  return indexDifference;
+}
+
 // function that plays the entire game. five rounds are played.
 async function playGame() {
   while (
@@ -149,7 +164,7 @@ async function playGame() {
     console.log("You chose: " + humanChoice);
 
     // calling the "animation" function
-    let promiseResult = await countdown(choices);
+    let promiseResult = await renderAnimation(choices);
     console.log(promiseResult);
 
     // separated the game logic and the UI by moving the score tracking to the UI
@@ -164,8 +179,13 @@ async function playGame() {
     }
 
     gamesPlayed++;
+
+    // hold for about one second before clearing
+    await sleep(1000);
+    clear();
   }
 
+  // Show the final result when exiting the game
   renderStatus();
 }
 
